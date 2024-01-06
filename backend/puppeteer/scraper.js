85bb3356f0db85ad65dcff1d2022e13bb5b02ffb
Paul <paulnguyen0417@gmail.com>
2024-01-06 08:57:21 -0500

    Complete Webscraping Functionality
    - Extracted title + link from indeed.com job cards and used title to click on the cards which revealed details
    - Extracted details to an object and appended it to an array of objects containing jobs
    - Printed array to json file
    - Added pagination and limit to amount of jobs wanted by query

--- backend/puppeteer/scraper.js
+++ backend/puppeteer/scraper.js
@@ -8,20 +8,45 @@ const { executablePath } = require('puppeteer')
 // Websites
 const website = 'https://ca.indeed.com'
 
-// Scrape Titles Function w/ Pagination
-const scrapeTitles = async (page, amount) => {
-    let titles = []
+// Scraper Function
+const scrape = async (url) => {
+    const browser = await puppeteer.launch({ headless: 'new', executablePath: executablePath() })
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage()
+    await page.goto(`${url}/jobs?q=Developer&l=Waterloo`)
 
-    while (titles.length < amount) {
+    const jobs = []
+    const amount = 3
+    while (jobs.length < amount) {
         await page.waitForSelector('span');
+
         const extractedData = await page.evaluate(async () => {
-            const titlesData = Array.from(document.querySelectorAll('.jcs-JobTitle'))
-            return titlesData.map((item) => item.querySelector('span').innerText)
+            const data = Array.from(document.querySelectorAll('.jcs-JobTitle'))
+            return data.map((item) => ({
+                title: item.querySelector('span').innerText,
+                link: item.getAttribute('href')
+            }))
         })
 
         for (const data of extractedData) {
-            if (titles.length < amount) {
-                titles.push(data)
+            await page.click(`span[title="${data.title}"`)
+            await page.waitForTimeout(1500)
+
+            const jobObject = await page.evaluate(async (data, url) => {
+                const job = document.querySelector('.fastviewjob')
+                const jobObject = {
+                    title: data.title,
+                    company: job.querySelector('.css-1f8zkg3.e19afand0').innerText,
+                    location: job.querySelector('div[data-testid="inlineHeader-companyLocation"] div').innerText,
+                    description: job.querySelector('#jobDescriptionText').innerText,
+                    link: url + data.link
+                }
+
+                return jobObject
+            }, data, url)
+
+            if (jobs.length < amount) {
+                jobs.push(jobObject)
             } else {
                 break;
             }
@@ -39,19 +64,7 @@ const scrapeTitles = async (page, amount) => {
         }
     }
 
-    return titles
-}
-
-// Scraper Function
-const scrape = async (url) => {
-    const browser = await puppeteer.launch({ headless: 'new', executablePath: executablePath() })
-    const context = await browser.createIncognitoBrowserContext();
-    const page = await context.newPage()
-    await page.goto(`${url}/jobs?q=Developer&l=Waterloo`)
-
-    const titles = await scrapeTitles(page, 25)
-
-    fs.writeFile('titles.json', JSON.stringify(titles), (error) => {
+    fs.writeFile('jobs.json', JSON.stringify(jobs), (error) => {
         if (error) throw error;
         console.log('File saved')
     })

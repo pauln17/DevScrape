7c81e1fa6b32a3c54f5439e503a2522d54da5ab8
Paul <paulnguyen0417@gmail.com>
2024-01-12 02:13:49 -0500

    Add deployed render backend url as baseUrl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -58,7 +58,9 @@ function App() {
       <div style={{ textAlign: 'center', margin: '2rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start' }}>
         <h1 style={{ color: 'white', fontWeight: '1000', margin: 0, fontSize: '36px' }}>Webscraper for Developer Listings</h1>
         <h3 style={{ color: '#c6d0cd', fontWeight: '300', margin: 0, fontSize: '24px' }}>An automated and personalized webscraper that retrieves the latest data on available developer-related job listings daily at 10AM EST</h3>
-        <h1 style={{ color: '#c6d0cd', fontWeight: '300', marginTop: '15px', fontSize: '24px' }}>{time.hours}:{time.minutes}:{time.seconds}</h1>
+        {time &&
+          <h1 style={{ color: '#c6d0cd', fontWeight: '300', marginTop: '15px', fontSize: '24px' }}>{time.hours}:{time.minutes}:{time.seconds}</h1>
+        }
       </div>
       {jobs.length > 0 ?
         <div style={{ boxShadow: '0 0 15px 25px rgba(5, 5, 5, 0.5)', borderRadius: '10px', display: 'flex', marginBottom: '25px', border: '2px solid rgb(200, 200, 200)', justifyContent: 'center', height: '100%', width: '85%', overflowY: 'scroll' }}>
--- frontend/src/services/jobs.js
+++ frontend/src/services/jobs.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/jobs'
+const baseUrlDeply = 'https://job-webscraper-backend.onrender.com/api/jobs'
 
 const getAll = async () => {
-    const response = await axios.get(baseUrl)
+    const response = await axios.get(baseUrlDeply)
     return response.data
 };
 

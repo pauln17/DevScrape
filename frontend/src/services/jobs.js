e4e1f114a7f9f6d02dca035adc8899fb11703c60
Paul <paulnguyen0417@gmail.com>
2024-01-13 12:50:02 -0500

    Recreate production build folder

--- frontend/src/services/jobs.js
+++ frontend/src/services/jobs.js
@@ -4,6 +4,7 @@ const baseUrl = process.env.REACT_APP_MODE === 'development'
     : process.env.REACT_APP_BASEURLDEPLOY
 
 const getAll = async () => {
+    console.log(baseUrl, process.env.REACT_APP_MODE)
     const response = await axios.get(baseUrl)
     return response.data
 };
